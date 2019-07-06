a277996a8b33e737483cfded6582477849f7bc58
Newforce <thecreativefitz@gmail.com>
2019-07-06 09:50:06 -0400

    Added search functionality for courses index filtering by title

--- nucSummary/Controllers/CoursesController.cs
+++ nucSummary/Controllers/CoursesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -13,19 +14,30 @@ namespace nucSummary.Controllers
 {
     public class CoursesController : Controller
     {
-        private readonly ApplicationDbContext _context;
 
-        public CoursesController(ApplicationDbContext context)
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CoursesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
             _context = context;
         }
 
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
         // GET: Courses
         [Authorize]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchQuery)
         {
-            var applicationDbContext = _context.Courses.Include(c => c.ApplicationUser);
-            return View(await applicationDbContext.ToListAsync());
+            ApplicationUser user = await GetCurrentUserAsync();
+            List<Courses> courseList = await _context.Courses
+                .ToListAsync();
+            if(searchQuery != null)
+            {
+                courseList = courseList.Where(course => course.Title.Contains(searchQuery)).ToList();
+            }
+            return View(courseList);
         }
 
         // GET: Courses/Details/5
@@ -63,6 +75,8 @@ namespace nucSummary.Controllers
         {
             if (ModelState.IsValid)
             {
+                ApplicationUser user = await GetCurrentUserAsync();
+                courses.ApplicationUserId = user.Id;
                 _context.Add(courses);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
